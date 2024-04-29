db757e80c18e2ac927473d1533933ae8a34826d9
Bojan SKI <codeucenje@gmail.com>
2024-04-29 15:58:08 +0200

    navigation component done (css will be done later). toggle theme (dark/light mode) func done. footer component done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,49 @@
+import { NavLink } from "react-router-dom"
+import { BsFillSunFill, BsFillMoonFill } from "react-icons/bs"
+import { useState } from "react"
 
 const Navbar = () => {
+  const [isDarkTheme, setIsDarkTheme] = useState(true)
+
+  const toggleTheme = () => {
+    setIsDarkTheme(() => !isDarkTheme)
+    document.body.classList.toggle('dark-theme')
+  }
+
   return (
-    <div>Navbar</div>
+    <header>
+      <nav class="navbar bg-body-tertiary p-3">
+        {/* logo */}
+        <div className="logo">
+          <a class="navbar-brand" href="/">
+            Recipe Rescue App
+          </a>
+        </div>
+
+        {/* navigation */}
+        <ul className="navbar-nav d-flex flex-row">
+          <li className="nav-item mx-2">
+            <NavLink to="/" className="nav-link">
+              Home
+            </NavLink>
+          </li>
+          <li className="nav-item mx-2">
+            <NavLink to="search" className="nav-link">
+              Advance Search
+            </NavLink>
+          </li>
+        </ul>
+
+        {/* toggle theme btn */}
+        <button className="btn-toggle" onClick={toggleTheme}>
+          {isDarkTheme ? (
+            <BsFillSunFill />
+          ) : (
+            <BsFillMoonFill />
+          )}
+        </button>
+      </nav>
+    </header>
   )
 }
 
